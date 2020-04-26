1f5fd851a9b630b935089928d6b216c6a1cc06ff
lbirts <birtslauren@gmail.com>
2020-04-26 10:22:05 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def lifters
+    memberships.map {|membership| membership.lifter}
+  end
+
+  def lifters_name
+    lifters.map {|lifter| lifter.name}
+  end
+
+  def lift_total
+    total = 0
+    lifters.each {|lifter| total += lifter.lift_total}
+    total
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,34 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+stan = Lifter.new("Stanley", 300)
+moe = Lifter.new("Maurice", 350)
+bran = Lifter.new("Brandon", 250)
+
+planet = Gym.new("Planet Fitness")
+anytime = Gym.new("Anytime Fitness")
+twofour = Gym.new("24hr")
+
+memb1 = Membership.new(20, stan, planet)
+memb2 = Membership.new(40, moe, anytime)
+memb3 = Membership.new(30, bran, planet)
+memb3 = Membership.new(15, moe, twofour)
+
+Lifter.all
+Gym.all
+Membership.all
+
+moe.memberships
+moe.gyms
+moe.total_cost
+bran.create_membership(20, twofour)
+
+Lifter.average_lift_total
+
+planet.memberships
+twofour.lifters
+twofour.lifters_name
+planet.lift_total
 
 binding.pry
 
