b27061736c2b3c1114592752e4b414d78b0d9585
devchea <rchea817@gmail.com>
2020-04-28 13:30:54 -0700

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,53 @@
+# XGet a list of all lifters - Lifter.all
+# XGet a list of all the memberships that a specific lifter has - lifter_membership
+# XGet a list of all the gyms that a specific lifter has memberships to - lifter_gym_membership
+# XGet the average lift total of all lifters - self.average_of_all_lifters
+# XGet the total cost of a specific lifter's gym memberships - total_cost_of_memberships
+# XGiven a gym and a membership cost, sign a specific lifter up for a new gym - signup
+
 class Lifter
   attr_reader :name, :lift_total
+
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def lifter_membership #l1.lifter_membership
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def lifter_gym_membership #l1
+    lifter_membership.map {|membership| membership.gym}
+  end
+
+  def self.average_of_all_lifters
+    total_lift_weight = 0
+    number_of_lifters = self.all.count
+    self.all.each do |lifter|
+      total_lift_weight += lifter.lift_total
+    end
+    (total_lift_weight/number_of_lifters).to_f
+  end
+
+  def total_cost_of_memberships
+    total_cost = 0
+    lifter_membership.each do |membership|
+      total_cost += membership.cost
+    end
+      total_cost
+  end
+
+  def signup(cost, gym) #l2.signup(g2, 40)
+    Membership.new(cost, self, gym)
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,28 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+l1 = Lifter.new("lname1", 21)
+l2 = Lifter.new("lname2", 321)
+l3 = Lifter.new("lname3", 121)
+l3 = Lifter.new("lname4", 151)
+l4 = Lifter.new("lname5", 31)
+l5 = Lifter.new("lname6", 211)
+g1 = Gym.new("gname1")
+g2 = Gym.new("gname2")
+g3 = Gym.new("gname3")
+
+
+m1 = Membership.new(13, l1, g1)
+m2 = Membership.new(12, l2, g1)
+m3 = Membership.new(31, l3, g3)
+m4 = Membership.new(14, l4, g2)
+m5 = Membership.new(13, l5, g2)
+m6 = Membership.new(8, l5, g2)
+m7 = Membership.new(2, l5, g1)
+
+
+
+
 binding.pry
 
 puts "Gains!"
